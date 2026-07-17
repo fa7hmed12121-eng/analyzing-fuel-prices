6948ff703ff4da8008f3d21049427d011834e7d6
Fatima <fa7hmed12121@gmail.com>
2026-07-17 10:19:39 +0300

    low subsidy fuel much priced

--- analyze_fuels.sql
+++ analyze_fuels.sql
@@ -29,7 +29,6 @@ SELECT
     --ROUND(AVG(diesel_usd_liter),2) diesel_prices,
     ROUND(AVG(tax_percentage),2) taxes
 FROM global_fuel_prices_2020_2026   
-WHERE country Is ''     
 GROUP BY subsidy_level, region
 ORDER BY  region
 
@@ -41,27 +40,89 @@ ORDER BY  region
  */
 
  SELECT
+    subsidy_level,
     Year(date) the_year,
-    MONTH(date) the_month,
+    --MONTH(date) the_month,
     ROUND(AVG(petrol_usd_liter),2) ptrol_prices,
+    ROUND(AVG(diesel_usd_liter),2) diesel_prices,
+    ROUND(AVG(lpg_usd_liter),2) lpg_prices,
+    ROUND(AVG(brent_crude_usd),2) brent_prices,
     ROUND(AVG(tax_percentage),2) taxes
 
 FROM global_fuel_prices_2020_2026
-GROUP BY year(date)  , MONTH(date) 
-ORDER BY the_year, the_month
+WHERE region = 'South America'
+GROUP BY year(date)  , subsidy_level
+ORDER BY the_year
 
 /* Count to countries of each subside level*/
 
 SELECT
-    region,
-    subsidy_level,
-    COUNT(distinct(country)) quantity_of_countries,
-    ROUND(AVG(petrol_usd_liter),2) pertol_prices
+    country,
+    subsidy_level, income_level,
+   -- COUNT(distinct(country)) quantity_of_countries,
+    ROUND(AVG(petrol_usd_liter),2) pertol_prices,
+    ROUND(AVG(diesel_usd_liter),2) diesel_prices,
+    ROUND(AVG(lpg_usd_liter),2) lpg_prices,
+    ROUND(AVG(brent_crude_usd),2) brent_prices,
+    ROUND(AVG(tax_percentage),2) taxes
 
 FROM global_fuel_prices_2020_2026    
-GROUP BY region, subsidy_level
+GROUP BY country, subsidy_level, income_level
+ORDER BY country
+
+/* 
+ The average prices of country with L
+ its income_leve */
+
+ SELECT
+    region,
+    income_level,
+    COUNT(distinct(country)) countries,
+    ROUND(AVG(petrol_usd_liter),2) pertol_prices,
+    ROUND((AVG(lpg_usd_liter)),2) lpq_prices
+
+FROM global_fuel_prices_2020_2026    
+GROUP BY region, income_level
 ORDER BY region
 
+/* --------------------------
+Find the presantage of prices 
+increasing through years ---*/
 
+With yearly_prices As (SELECT a_year, 
+    avg_petrol,
+    avg_lpq,
+    ISNULL(pre_avg_petrol, 0) pre_avg_petrol,
+    ISNULL(pre_avg_lpq, 0) pre_avg_lpq
 
- 
+FROM
+(SELECT *,
+    LAG(avg_petrol) OVER(ORDER BY a_year) pre_avg_petrol,
+    LAG(avg_lpq) Over(ORDER BY a_year) pre_avg_lpq
+
+FROM(SELECT 
+   
+    YEAR(date) a_year,
+    ROUND(AVG(petrol_usd_liter),2) avg_petrol,
+    ROUND(AVG(lpg_usd_liter),2) avg_lpq
+
+FROM global_fuel_prices_2020_2026    
+GROUP BY YEAR(date)
+ )f
+ )n
+ )
+SELECT a_year ,
+    avg_petrol,
+  CONCAT(ROUND((petrol_increasing),3), '%') increasing_presantage,
+ avg_lpq, 
+  CONCAT(ROUND((lpq_increasing),3), '%') increasing_presantage
+
+FROM( Select 
+    a_year,
+    avg_petrol , 
+    (pre_avg_petrol / avg_petrol ) /100 as petrol_increasing,
+    avg_lpq,
+    (pre_avg_lpq / avg_lpq)/100 as lpq_increasing
+
+    
+ FROM yearly_prices )f
